5692969c03d77c49492c7ec18f18955631eeb4c4
WilsonGoins <wilson.goins@ufl.edu>
2026-04-07 15:04:28 -0400

    removed dev indicators

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
+  
   // Allow the page to be embedded in Canvas iframes
   async headers() {
     return [
